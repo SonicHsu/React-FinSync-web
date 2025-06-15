144e7eafd079622b01d41904fb8c58cc21cb2c58
SonicHsu <soniccz@gmail.com>
2025-06-15 19:23:35 +0800

    RWD基本款完成 開始進行typescript

--- components/MonthlySummaryItem.jsx
+++ components/MonthlySummaryItem.jsx
@@ -1,8 +1,8 @@
 export default function MonthlySummaryItem({ label, value, valueBgColor, valueColor }) {
   return (
-    <div className={`flex  min-w-[100px] sm:min-w-[220px] sm:w-full items-center justify-between rounded-[10px] border border-white/10  px-5 ${valueBgColor}`}>
+    <div className={`flex sm:flex-col lg:flex-row  min-w-[100px] lg:min-w-[220px]  sm:w-full sm:min-w-[160px] items-center justify-between rounded-[10px] border border-white/10  px-5 ${valueBgColor}`}>
       <span className="text-xl lg:text-2xl font-bold">{label}</span>
-      <span className={`text-xl lg:text-3xl font-bold ${valueColor}`}>{value}</span>
+      <span className={`text-xl sm:text-2xl lg:text-3xl font-bold ${valueColor}`}>{value}</span>
     </div>
   );
 }
